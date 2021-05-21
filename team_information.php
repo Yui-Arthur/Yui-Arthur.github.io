89307125c6d54f4656d4d23811b895a1f451ecb0
Yui <arthur80633@gmail.com>
2021-05-22 03:28:58 +0800

    498

--- team_information.php
+++ team_information.php
@@ -9,7 +9,7 @@
 	<link rel="icon" href="favicon.jpg" type="image/x-icon">
   
     
-    <link rel="stylesheet" href="static/css/style.css">
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -19,7 +19,7 @@
 	
 	
 	
-	
+	<link rel="stylesheet" href="static/css/style.css">
 	
     
     <link href="static/css/all.css" rel="stylesheet">
