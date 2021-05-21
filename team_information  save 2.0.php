9f923462c6c6863c5445e03ff324337b3a6ca6b7
Yui <arthur80633@gmail.com>
2021-05-21 18:57:23 +0800

    5646

--- team_information  save 2.0.php
+++ team_information  save 2.0.php
@@ -78,9 +78,8 @@
 		
 		<!-- 滑動介面 -->
 		<div class="swiper-container swiper-container-initialized swiper-container-horizontal">
-
-			<div class="swiper-wrapper ">
-			<div class="swiper-slide swiper-slide-active" style="width: 645px; margin-right: 30px; ">
+		<div class="swiper-wrapper mb-3">
+			<div class="swiper-slide swiper-slide-active" style="width: 645px; margin-right: 30px;">
 				<div class="card shadow" style="border-radius: 25px ;background-color:rgba(255,255 ,255 , 0.75);">
 				<!--<div class="tile-progress tile-pink shadow" style="border-radius: 25px;">-->
 					<div class="tile-header" style="padding: 20px;">
@@ -106,7 +105,7 @@
 					
 				</div>
 			</div>
-			<div class="swiper-slide swiper-slide-next" style="width: 645px; margin-right: 30px; " >
+			<div class="swiper-slide swiper-slide-next" style="width: 645px; margin-right: 30px;">
 				<div class="tile-progress tile-pink shadow" style="border-radius: 25px;">
 					<div class="card shadow" style="border-radius: 25px ;background-color:rgba(255,255 ,255 , 0.75);">
 						<div class="tile-header" style="padding: 20px;">
@@ -117,7 +116,7 @@
 					
 				</div>
 			</div>
-			<div class="swiper-slide swiper-slide-next" style="width: 645px; margin-right: 30px; ">
+			<div class="swiper-slide swiper-slide-next" style="width: 645px; margin-right: 30px;">
 				<div class="tile-progress tile-pink shadow" style="border-radius: 25px;">
 					<div class="card shadow" style="border-radius: 25px ;background-color:rgba(255,255 ,255 , 0.75);">
 							<div class="tile-header" style="padding: 20px;">
@@ -126,15 +125,10 @@
 					</div>
 				
 					
-					</div>
 				</div>
 			</div>
-		</div
+		</div>
 		
-		
-	<!-- 分頁標籤 -->
-	<div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 2"></span></div>
-	<span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
 		
 		
 	
@@ -142,8 +136,39 @@
         <div class="card shadow" style="border-radius: 25px ;background-color:rgba(255,255 ,255 , 0.75);">
             
                 
-                   
+                    <div class="card-body">
 					
+					
+					
+					
+					
+					
+					
+					
+					
+					<?php
+					//顯示金幣
+					$link=pg_connect("$host $port $dataname $user $password");
+					$sql="SELECT coin_number FROM team_coin WHERE team='".$_SESSION['team']."'";
+					
+					
+					$result=pg_query($link,$sql);
+					if($result)
+					{
+						$row_result=pg_fetch_assoc($result);
+						echo "<p class='h2' > <img class='mb-4' src='image/coin.jpg' alt='' width='100' height='100'>x" .$row_result['coin_number']."</p>";
+					}
+					else
+						echo "連接錯誤";
+					
+					pg_close($link);
+					?>
+					
+					
+					
+					<br>
+					<p class='h3' > <img class='mb-4' src='image/flag.png' alt='' width='100' height='100'>總佔領時間</p>
+					<br>
 					<p class='h3' > <img class='mb-4' src='image/boo.jpg' alt='' width='100' height='100'>夜教</p>
 					<br>
 					<p class='h3' > <img class='mb-4' src='image/fish.jpg' alt='' width='100' height='100'>水大地 </p>
@@ -165,7 +190,9 @@
 	<button class="w-10 btn btn-lg btn-primary" type="submit" style="background-color:#f7f7f8;color: black;">	<a href="logout.php">登出</a></button>
 
 
-	
+	<!-- 分頁標籤 -->
+	<div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 2"></span></div>
+	<span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
 
 	<!-- 初始化滑動介面 -->
 	<script>
